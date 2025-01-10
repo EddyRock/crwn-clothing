b48280862f086d21f56a9774ed309641154f9cc6
Eduard Rudniev <eduardrudnev97@gmail.com>
2025-01-10 21:03:52 +0100

    Fix eslint + start using reducers

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,27 @@
-/** @format */
-
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact, { rules } from "eslint-plugin-react";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      globals: globals.browser
+    },
+    plugins: {
+      prettier: prettierPlugin
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'on'
+    }
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-  {
-    extends: [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "plugin:prettier/recommended",
-    ],
-    rules: {
-      "react/react-in-jsx-scope": "off",
-    },
-  },
+  prettierConfig
 ];
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -53,11 +53,32 @@ export const CartContext = createContext({
   totalCount: 0
 });
 
+const INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  cartCount: 0,
+  totalCount: 0
+};
+
+const cartReducer = (state, action) => {
+  const { type, payload } = action ?? {};
+  switch (type) {
+    case 'SET_CART_ITEMS':
+      return {
+        ...state,
+        ...payload
+      };
+
+    default:
+      throw new Error(`Unhandled action type in cartReducer: ${type}`);
+  }
+};
+
 export const CartProvider = ({ children }) => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
-  const [cartCount, setCartCount] = useState(0);
-  const [totalCount, setTotalCount] = useState(0);
+  const [{ isCartOpen, cartItems, cartCount, totalCount }, dispatch] = useReducer(
+    cartReducer,
+    INITIAL_STATE
+  );
 
   useEffect(() => {
     const newCartCount = cartItems.reduce((total, { quantity }) => {
